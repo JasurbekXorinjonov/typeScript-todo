8856dd6e2ca4a4312442ed5ae523defa16e7a2d9
JasurbekXorinjonov <130471952+JasurbekXorinjonov@users.noreply.github.com>
2023-10-17 20:18:32 +0500

    Todo tayor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
+import { ChangeEvent, useState } from "react";
+import styles from "./home.module.css";
+import { data } from "./components/contstants";
+import { IData } from "./components/interfaces";
+
 function App(): JSX.Element {
-  return <div>App</div>;
+  const [title, setTitle] = useState<string>();
+  const [arr, setArr] = useState<IData[]>(data);
+
+  const changeHandler = (evt: ChangeEvent<HTMLInputElement>): void => {
+    setTitle(evt.target.value);
+  };
+
+  const handleSubmit = (): void => {
+    if (!title?.length) return;
+    const newData = {
+      title: title,
+      id: new Date().getTime(),
+      description: "description",
+    };
+    console.log(newData);
+    setArr([...arr, newData]);
+    setTitle("");
+  };
+
+  const dleteItem = (id: number): void => {
+    const newData = arr.filter((i) => i.id != id);
+    setArr(newData);
+  };
+
+  return (
+    <div className={styles.todo}>
+      <h1 className={styles.titel}>Ap Todo</h1>
+      <input
+        placeholder="Enter todo"
+        className={styles.input}
+        value={title}
+        onChange={changeHandler}
+      />
+      <button onClick={handleSubmit} className={styles.button}>
+        Add todo
+      </button>
+      <div className={styles.card}>
+        {arr.map((i) => (
+          <div key={i.id} className={styles.cardItem}>
+            <p>{i.title}</p>
+            <div className={styles.btn}>
+              <button onClick={() => dleteItem(i.id)}>Del</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/contstants/index.tsx
@@ -0,0 +1,7 @@
+import { IData } from "../interfaces";
+
+export const data: IData[] = [
+  { title: "Axmad", id: 1, description: "Descriptiton" },
+  { title: "Usmon", id: 2, description: "Descriptiton" },
+  { title: "Nosir", id: 3, description: "Descriptiton" },
+];
